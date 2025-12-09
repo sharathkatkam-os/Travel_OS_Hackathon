e14589ff43b627b8cd7a89bb86a998a26fe74695
Sharath Kumar Katkam <sharathkatkam@gmail.com>
2025-12-09 20:58:39 +0530

    Updated NewTrip.tsx

--- src/pages/NewTrip.tsx
+++ src/pages/NewTrip.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 
 export default function NewTrip() {
