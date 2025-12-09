fcf48d230aa2f070ade98fe0fd046143f28be5d5
Sharath Kumar Katkam <sharathkatkam@gmail.com>
2025-12-09 21:19:53 +0530

    Updated TripOverview.tsx

--- src/pages/TripOverview.tsx
+++ src/pages/TripOverview.tsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { Button } from "../components/ui/button";
+import { Button } from "../components/ui/Button";
 
 
 export default function TripOverview() {
