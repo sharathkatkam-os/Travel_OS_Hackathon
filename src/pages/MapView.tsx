7551b105b1bdb37c4e22b7626f2a9696c583c6cf
Sharath Kumar Katkam <sharathkatkam@gmail.com>
2025-12-09 21:36:51 +0530

    Updated MapView.tsx

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { ArrowLeft, MapPin } from 'lucide-react';
-import Card from '../components/ui/Card';
+import { Card }from '../components/ui/Card';
 import { useTravel } from '../context/TravelContext';
 
 export default function MapView() {
