2cd8decf5f1713fa514a9dbf804a758dd1d4168b
Sharath Kumar Katkam <sharathkatkam@gmail.com>
2025-12-09 21:13:20 +0530

    Updated AddActivity.tsx

--- src/pages/AddActivity.tsx
+++ src/pages/AddActivity.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Button } from "../components/button";
-import { Input } from "../components/input";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 
 
 export default function AddActivity() {
